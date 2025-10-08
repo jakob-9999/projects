a5e8ba12d84890b1de521e5e9b0f971292c19300
Jakob <jdj1994@gmail.com>
2025-10-08 19:46:29 +0200

    Initial commit with Spring Boot project

---
+++ src/test/java/com/project/AAU_SW3_P3/AauSw3P3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.AAU_SW3_P3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AauSw3P3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
