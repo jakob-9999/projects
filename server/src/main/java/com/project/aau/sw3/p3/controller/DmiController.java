4550cf4cf55f9feb5611b5f53fbbc3b6d9666bb5
Sebastian-Jensen1 <hm12nx@student.aau.dk>
2025-10-15 15:06:44 +0200

    Get data from DMI API + create packages

--- server/src/main/java/com/project/aau/sw3/p3/controller/DmiController.java
+++ server/src/main/java/com/project/aau/sw3/p3/controller/DmiController.java
@@ -1,4 +1,24 @@
 package com.project.aau.sw3.p3.controller;
 
+import com.project.aau.sw3.p3.service.DmiService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Tells Spring, this class hadnles http request
 public class DmiController {
+
+    private final DmiService dmiService;
+
+    // Constructor injection – Spring sørger for at give os en instans af DmiService
+    public DmiController(DmiService dmiService) {
+        this.dmiService = dmiService;
+    }
+
+    // GET-endpoint, som du kan kalde i browseren
+    @GetMapping("/api/dmi")
+    public String getDmiData() {
+        // Calls fetchDmiData from DmiService and that method returns json, why it shows in browser
+        return dmiService.fetchDmiData();
+    }
 }
+
