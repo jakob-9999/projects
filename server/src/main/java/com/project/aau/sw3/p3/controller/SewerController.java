163276dc11a8af8a79d43914f40275d676600f94
NikiRyomHansen <nikiryom@gmail.com>
2025-11-05 14:43:57 +0100

    Removed unused endpoints

--- server/src/main/java/com/project/aau/sw3/p3/controller/SewerController.java
+++ server/src/main/java/com/project/aau/sw3/p3/controller/SewerController.java
@@ -2,30 +2,17 @@ package com.project.aau.sw3.p3.controller;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.project.aau.sw3.p3.service.SewerFeatureService;
-import com.project.aau.sw3.p3.service.SewerService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.project.aau.sw3.p3.model.SewerData;
-import com.project.aau.sw3.p3.repository.SewerDataRepo;
-import java.util.List;
 
 @RestController
 public class SewerController {
 
-    private final SewerService sewerService;
     private final SewerFeatureService sewerFeatureService;
-    private final SewerDataRepo sewerDataRepo;
 
-    public SewerController(SewerService sewerService, SewerFeatureService sewerFeatureService, SewerDataRepo sewerDataRepo) {
-        this.sewerService = sewerService;
+    public SewerController(SewerFeatureService sewerFeatureService) {
         this.sewerFeatureService = sewerFeatureService;
-        this.sewerDataRepo = sewerDataRepo;
-    }
-
-    @PostMapping(value= "/api/sewageland", produces= "application/json")
-    public String getSewageFromService() throws Exception {
-        return sewerService.saveAndGetSewageLand();
     }
 
     @PostMapping( "/api/sewageland/features")
@@ -36,15 +23,5 @@ public class SewerController {
     @GetMapping("/api/sewageland/features")
     public ObjectNode getSewagelandFeatureCollection() {
         return sewerFeatureService.buildSewerFeatureCollection();
-    }
-
-    @GetMapping("/api/sewerdata")
-    //returns list of sewerData objects
-    public List<SewerData> getSavedSewerData() {
-
-        //sewerDataRepo.findAll() is a method that makes SQL request like SELECT * FROM sewer_data
-        //it maps each row to sewerData object
-        //collects all objects in List<SewerData>
-        return sewerDataRepo.findAll();
     }
 }
