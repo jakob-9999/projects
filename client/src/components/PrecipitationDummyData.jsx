cf88d10bbb475aab74a599f79aeec9b7b543da88
Jakob <jdj1994@gmail.com>
2025-11-08 07:50:25 +0100

    frontend for grid cell vizualisation

--- client/src/components/PrecipitationDummyData.jsx
+++ client/src/components/PrecipitationDummyData.jsx
@@ -1,54 +1,50 @@
 import { CircleMarker, Popup } from "react-leaflet";
+import {useEffect} from "react";
+import {useState} from "react";
+import {geoJSON} from "leaflet";
 
-// Dummy data for precipitation layer
-// The function returns a list of precipitation points with their intensity and time.
+// fetch grid cells
 export default function PrecipitationLayer() {
-    const precipitationData = [
-        { lat: 56.16, lon: 10.20, intensity: 0, time: "2025-10-24 12:00" },
-        { lat: 56.18, lon: 10.22, intensity: 3, time: "2025-10-24 12:00" },
-        { lat: 56.14, lon: 10.19, intensity: 8, time: "2025-10-24 12:00" },
-        { lat: 56.20, lon: 10.25, intensity: 12, time: "2025-10-24 12:00" },
-        { lat: 56.20, lon: 10.11, intensity: 15, time: "2025-10-24 12:00" },
-        { lat: 56.17, lon: 10.23, intensity: 20, time: "2025-10-24 12:00" },
-        { lat: 56.13, lon: 10.15, intensity: 25, time: "2025-10-24 12:00" },
-        { lat: 56.19, lon: 10.13, intensity: 30, time: "2025-10-24 12:00" },
-        { lat: 56.14, lon: 10.20, intensity: 35, time: "2025-10-24 12:00" },
-        { lat: 56.20, lon: 10.10, intensity: 40, time: "2025-10-24 12:00" },
-        { lat: 56.15, lon: 10.21, intensity: 45, time: "2025-10-24 12:00" },
-        { lat: 56.17, lon: 10.07, intensity: 6, time: "2025-10-24 12:00" },
-        { lat: 56.13, lon: 10.12, intensity: 10, time: "2025-10-24 12:00" },
-        { lat: 56.16, lon: 10.20, intensity: 15, time: "2025-10-24 12:00" },
-        { lat: 56.15, lon: 10.20, intensity: 20, time: "2025-10-24 12:00" },
-        { lat: 56.15, lon: 10.21, intensity: 25, time: "2025-10-24 12:00" },
-        { lat: 56.16, lon: 10.19, intensity: 30, time: "2025-10-24 12:00" },
-        { lat: 56.17, lon: 10.20, intensity: 35, time: "2025-10-24 12:00" },
-    ];
+    const [precipitationData, setPrecipitationData] = useState([])
 
-    const getColor = (intensity) => {
-        if (intensity > 10) return "blue";
-        if (intensity > 5) return "dodgerblue";
-        if (intensity > 0.5) return "lightblue";
+    useEffect(() => {
+        fetch("/api/dmi-dini-bbox/get-grid")
+            .then((res) => {
+                return res.json()
+            })
+            .then(data => {
+                console.log(data)
+                setPrecipitationData(data.features)
+            })
+            .catch((err) => console.error("Error fetching geoJSON:", err));
+    }, []);
+
+    const getColor = (precipitation) => {
+        if (precipitation > 10) return "blue";
+        if (precipitation > 5) return "dodgerblue";
+        if (precipitation > 0.5) return "lightblue";
         return "gray";
     };
 
     return (
         <>
-            {precipitationData.map((point, i) => (
+            {precipitationData.map((data, i) => (
                 <CircleMarker
                     key={i}
-                    center={[point.lat, point.lon]}
+                    //order of coordinates is opposite of GeoJson-format (lat, long) because leaflet expects (long, lat)
+                    center={[data.geometry.coordinates[1], data.geometry.coordinates[0]]}
                     radius={10}
                     pathOptions={{
-                        color: getColor(point.intensity),
-                        fillColor: getColor(point.intensity),
+                        color: "blue",
+                        fillColor: "blue",
                         fillOpacity: 0.6,
                     }}
                 >
-                    <Popup>
-                        <b>Rain:</b> {point.intensity} mm/hr
+                    {<Popup>
+                        <b>Rain:</b> {data.properties["total-precipitation"]} mm/hr
                         <br />
-                        <b>Time:</b> {point.time}
-                    </Popup>
+                        <b>Time:</b> {data.properties.step}
+                    </Popup>}
                 </CircleMarker>
             ))}
         </>
