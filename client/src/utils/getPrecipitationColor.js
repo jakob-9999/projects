9d3631e0ebed7240eaa7f10df1865509f1ae36c0
Signe <snj2740@gmail.com>
2025-12-09 12:13:18 +0100

    changed color

--- client/src/utils/getPrecipitationColor.js
+++ client/src/utils/getPrecipitationColor.js
@@ -3,6 +3,6 @@ export function getPrecipitationColor(precipitation) {
         if (precipitation > 10) return "blue";
         if (precipitation > 5) return "dodgerblue";
         if (precipitation > 0.5) return "lightblue";
-        if (precipitation <= 0.5) return "dodgerblue"; //for test. remove later
+        if (precipitation <= 0.5) return "transparent"; //for test. remove later
         return "transparent";
 }
